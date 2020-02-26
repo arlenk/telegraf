e04a1780643256c2a78ed5aa95387fc2a00dbdc5
arlenk <arlen.khodadadi@gmail.com>
2020-02-26 04:09:39 +0000

    add udp handling

--- plugins/inputs/iperf3/iperf3.go
+++ plugins/inputs/iperf3/iperf3.go
@@ -16,13 +16,31 @@ var (
 )
 
 type Iperf3 struct {
+	// name of iperf3 binary file
 	Binary string
-	Hosts  []string `toml:"hosts"`
+
+	// iperf3 servers (ips or names) to connect to
+	Hosts []string
+
+	// protocol to test (tcp or udp)
+	Protocol string
+
+	// time in seconds to transmit for
+	TransmitTime string `toml:"transmit_time"`
 }
 
 var sampleConfig = `
-  binary = "iperf3"
+  ## iperf3 servers to test connections to  
   hosts = ["127.0.0.1"]
+
+  ## name or path of iperf3 binary
+  # binary = "iperf3"
+
+  ## protocol to test (tcp or udp)
+  # protocol = "tcp"
+
+  ## time in seconds to transmit for
+  # transmit_time = 10
 `
 
 func (f *Iperf3) Description() string {
@@ -36,54 +54,94 @@ func (f *Iperf3) SampleConfig() string {
 type tcpResult struct {
 	Total struct {
 		Sent struct {
-			Bytes   float64 `json:"bytes"`
-			Seconds float64 `json:"seconds"`
+			Bytes         float64 `json:"bytes"`
+			Seconds       float64 `json:"seconds"`
+			BitsPerSecond float64 `json:"bits_per_second"`
 		} `json:"sum_sent"`
 		Received struct {
-			Bytes   float64 `json:"bytes"`
-			Seconds float64 `json:"seconds"`
+			Bytes         float64 `json:"bytes"`
+			Seconds       float64 `json:"seconds"`
+			BitsPerSecond float64 `json:"bits_per_second"`
 		} `json:"sum_received"`
 	} `json:"end"`
 }
 
-func (f *Iperf3) Gather(acc telegraf.Accumulator) error {
-	binary := f.Binary
-	hosts := f.Hosts
+type udpResult struct {
+	Total struct {
+		Sum struct {
+			Bytes         float64 `json:"bytes"`
+			Seconds       float64 `json:"seconds"`
+			BitsPerSecond float64 `json:"bits_per_second"`
+			JitterMS      float64 `json:"jitter_ms"`
+			Packets       float64 `json:"packets"`
+			LostPackets   float64 `json:"lost_packets"`
+			LostPercent   float64 `json:"lost_percent"`
+		}
+	} `json:"end"`
+}
+
+func (ip *Iperf3) Gather(acc telegraf.Accumulator) error {
+	binary := ip.Binary
+	hosts := ip.Hosts
+	protocol := ip.Protocol
 
 	for _, host := range hosts {
-		var arg []string
-		args := append(arg, "-c", host, "-t", "1", "--json")
-		tags := make(map[string]string)
+		var args []string
+		args = append(args, "-c", host, "-t", "1", "--json")
+		if protocol == "udp" {
+			args = append(args, "-u")
+		}
+
 		cmd := execCommand(binary, args...)
 		out, err := cmd.Output()
 		if err != nil {
 			return fmt.Errorf("failed to run command %s: %s - %s", strings.Join(cmd.Args, " "), err, string(out))
 		}
-		var tcpRes tcpResult
-		json.Unmarshal(out, &tcpRes)
-		fmt.Println(tcpRes)
+
 		metrics := make(map[string]interface{})
-		metrics["sent_bps"] = tcpRes.Total.Sent.Bytes / tcpRes.Total.Sent.Seconds
-		metrics["received_bps"] = tcpRes.Total.Received.Bytes / tcpRes.Total.Received.Seconds
-		tags["host"] = host
+		if protocol == "tcp" {
+			var res tcpResult
+			json.Unmarshal(out, &res)
+			fmt.Println(res)
+			metrics["sent_bps"] = res.Total.Sent.BitsPerSecond
+			metrics["received_bps"] = res.Total.Received.BitsPerSecond
+		} else {
+			var res udpResult
+			json.Unmarshal(out, &res)
+			fmt.Printf("%+v", res)
+			metrics["bps"] = res.Total.Sum.BitsPerSecond
+			metrics["jitter_ms"] = res.Total.Sum.JitterMS
+			metrics["packets"] = res.Total.Sum.Packets
+			metrics["lost_packets"] = res.Total.Sum.LostPackets
+			metrics["lost_percent"] = res.Total.Sum.LostPercent
+		}
+
+		tags := map[string]string{
+			"host":     host,
+			"protocol": protocol,
+		}
 		acc.AddFields("iperf3", metrics, tags)
 	}
 	return nil
 }
 
 // Init ensures the plugin is configured correctly.
-func (p *Iperf3) Init() error {
-	path, err := exec.LookPath(p.Binary)
+func (ip *Iperf3) Init() error {
+	path, err := exec.LookPath(ip.Binary)
 	if err != nil {
 		return errors.New("could not find iperf3 binary")
 
 	}
 	//fmt.Printf("found binary: %s", path)
-	p.Binary = path
+	ip.Binary = path
 
 	//fmt.Printf("\nin Init() %+v\n", p)
-	if len(p.Hosts) < 1 {
+	if len(ip.Hosts) < 1 {
 		return errors.New("must specify at least one target host")
+	}
+
+	if (ip.Protocol != "tcp") && (ip.Protocol != "udp") {
+		return errors.New("protocol must be either udp or tcp")
 	}
 
 	return nil
@@ -92,7 +150,9 @@ func (p *Iperf3) Init() error {
 func init() {
 	// default values
 	f := Iperf3{
-		Binary: "iperf3",
+		Binary:       "iperf3",
+		Protocol:     "tcp",
+		TransmitTime: "10",
 	}
 
 	//fmt.Printf("\nin init(): %+v\n", f)
